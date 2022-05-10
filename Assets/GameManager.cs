d34bb66d0b07e5813c9da3243d527b972d29fbd9
JeldrikGaal <jeldrikgaal@gmail.com>
2022-05-10 13:43:00 +0200

    push again

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     public Graph mapRef;
     public GraphNode currentLocationRef;
     public bool first = false;
+    public int treesDestroyed;
     /*    public string currentLocationName;
     */
     void Awake() 
